3ec2cfc57bcb352ba2b28cf0c77f83d190e9a329
Rimsky <Rimsky@192.168.1.145>
2023-04-09 13:54:10 -0700

    commit Day2HWProject3

---
+++ src/com/HWProject3/DemoProyect3.java
@@ -0,0 +1,17 @@
+package com.HWProject3;
+
+public class DemoProyect3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("New test project3");
+		
+		System.out.println("New test project3");
+	}
+
+	public void sustraction(int a, int b) 	{
+		
+		System.out.println(10 - 5);
+
+	}
+}
